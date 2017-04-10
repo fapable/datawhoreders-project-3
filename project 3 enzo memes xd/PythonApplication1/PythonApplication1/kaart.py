cfbf1022ef558f3b8bcc649e3859950b5374784d
fapable <ramonvanek@outlook.com>
2017-04-10 22:04:38 +0200

    Add files via upload

--- project 3 enzo memes xd/PythonApplication1/PythonApplication1/kaart.py
+++ project 3 enzo memes xd/PythonApplication1/PythonApplication1/kaart.py
@@ -1,11 +1,18 @@
 from tkinter import *
 import colors as c
 
+
+
 tk = Tk()
 tk.resizable(width=False, height=False)
-width = tk.winfo_screenwidth()
-height = tk.winfo_screenheight()
+tk.title("memes")
+
+width = 1920
+height = 1080
 canvas = Canvas(tk, width=width, height=height)
+kaart = PhotoImage(file = "gsh.gif")
+
+canvas.create_image(1000,500,image=kaart)
 
 class polygon:
     def __init__(self,color,list):
@@ -23,5 +30,8 @@ feijenoord = polygon("blue",(1397,500,1375,506,1322,574,1262,614,1288,642,1378,6
 ijsselmonde = polygon("blue",(1572,594,1489,616,1469,593,1450,605,1496,698,1487,735,1454,735,1417,772,1457,824,1524,831,1574,813,1657,766,1666,779,1682,769,1712,606,1625,584))
 charlois = polygon("blue",(1262,614,1288,642,1378,617,1372,694,1361,698,1361,720,1417,772,1457,824,1388,820,1315,848,1315,869,1242,871,1161,839,1201,824,1218,800,1201,792,1199,763,1246,656,1225,646,1227,620))
 
+
+
 canvas.grid()
+
 tk.mainloop()
