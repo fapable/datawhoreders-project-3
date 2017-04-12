e9c02e70977d1579b2d716ba9952fc6d51db02e3
Ahmet Demir <admr61@gmail.com>
2017-04-12 20:57:53 +0200

    Add files via upload

---
+++ Politiebureaus.sql
@@ -0,0 +1,22 @@
+CREATE TABLE Politiebureaus (
+    
+Politiebureau VARCHAR(19),
+    
+Adres VARCHAR(29),
+    
+Wijk VARCHAR(19));
+
+INSERT INTO Politiebureaus VALUES ('Doelwater','Doelwater 5','Centrum');
+INSERT INTO Politiebureaus VALUES ('Zaagmolenstraat','Zaagmolenstraat 91','Noord');
+INSERT INTO Politiebureaus VALUES ('Noord','Pr. Frederik Hendrikstraat 40','Noord');
+INSERT INTO Politiebureaus VALUES ('Bureau Boezemsingel','Boezemsingel 12','Kralingen-Crooswijk');
+INSERT INTO Politiebureaus VALUES ('Taborstraat','Taborstraat 22','Kralingen-Crooswijk');
+INSERT INTO Politiebureaus VALUES ('Maashaven','Maashaven NZ 5','Feijenoord');
+INSERT INTO Politiebureaus VALUES ('Marconiplein','Schiedamseweg 400','Delfshaven');
+INSERT INTO Politiebureaus VALUES ('Overschie','Burg. Baumannlaan 180','Overschie');
+INSERT INTO Politiebureaus VALUES ('Zuidplein','Zuidplein 111','Charlois');
+INSERT INTO Politiebureaus VALUES ('De Veranda','Cor Kieboomplein 475','Feijenoord');
+INSERT INTO Politiebureaus VALUES ('Sandelingplein','Groene Hilledijk 275','Feijenoord');
+INSERT INTO Politiebureaus VALUES ('J.J.P. Oudsingel','Dr. JJP Oudsingel 1','Prins Alexander');
+INSERT INTO Politiebureaus VALUES ('Zevenkampse Ring','Zevenkampse Ring 340','Prins Alexander');
+INSERT INTO Politiebureaus VALUES ('Hoogvliet','Middenbaan Noord 63','Hoogvliet');
