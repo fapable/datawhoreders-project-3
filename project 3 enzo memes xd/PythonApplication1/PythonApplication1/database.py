68e259e0137b5fa9d091bf0cbc2ca88ab5324043
Ino van den Berg <0928787@hr.nl>
2017-04-12 21:06:53 +0200

    Add files via upload

--- project 3 enzo memes xd/PythonApplication1/PythonApplication1/database.py
+++ project 3 enzo memes xd/PythonApplication1/PythonApplication1/database.py
@@ -1,27 +1,27 @@
 import psycopg2
-  
-def interact_with_database(command):
-    connection = psycopg2.connect(host = "localhost", dbname = "Jaar 1 Project 3", user = "postgres", password = "DatThaBeast3636")
 
-    cursor = connection.cursor()
+class Database:  
+    def interact_with_database(command):
+        connection = psycopg2.connect(host = "localhost", dbname = "Jaar 1 Project 3", user = "postgres", password = "DatThaBeast3636")
+        cursor = connection.cursor()
 
-    # Execute the command
-    cursor.execute(command)
-    connection.commit()
+        # Execute the command
+        cursor.execute(command)
+        connection.commit()
 
-    # Save results
-    results = None
-    try:
-        results = cursor.fetchall()
-    except psycopg2.ProgrammingError:
-        # Nothing to fetch
-        pass
+        # Save results
+        results = None
+        try:
+            results = cursor.fetchall()
+        except psycopg2.ProgrammingError:
+            # Nothing to fetch
+            pass
 
-    # Close connection
-    cursor.close()
-    connection.close()
+        # Close connection
+        cursor.close()
+        connection.close()
 
-    return results
+        return results
 
-def get_data(tabel, jaar, wijk, result):
-    return interact_with_database("SELECT " + str(result) + " FROM " + str(tabel) + "\nWHERE jaar = " + str(jaar) + "AND wijk = " + str(wijk))[0][0]
+    def get_data(result, tabel, jaar, wijk):
+        return Database.interact_with_database("SELECT " + str(result) + " FROM " + str(tabel) + " WHERE jaar = " + str(jaar) + "AND wijk = " + str(wijk))[0][0]
