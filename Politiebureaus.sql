5c0d41f43be653ddb15558b770c4fa2e47e93bb6
Ahmet Demir <admr61@gmail.com>
2017-04-14 16:00:29 +0200

    Update Politiebureaus.sql

--- Politiebureaus.sql
+++ Politiebureaus.sql
@@ -6,7 +6,7 @@ Adres VARCHAR(29),
     
 Wijk VARCHAR(19),
 X INT,
-Y INT));
+Y INT);
 
 INSERT INTO Politiebureaus VALUES ('Doelwater','Doelwater 5','Centrum', 895, 305);
 INSERT INTO Politiebureaus VALUES ('Zaagmolenstraat','Zaagmolenstraat 91','Noord', 900, 260);
@@ -16,7 +16,7 @@ INSERT INTO Politiebureaus VALUES ('Taborstraat','Taborstraat 22','Kralingen-Cro
 INSERT INTO Politiebureaus VALUES ('Maashaven','Maashaven NZ 5','Feijenoord', 926, 390);
 INSERT INTO Politiebureaus VALUES ('Marconiplein','Schiedamseweg 400','Delfshaven', 780, 345);
 INSERT INTO Politiebureaus VALUES ('Overschie','Burg. Baumannlaan 180','Overschie', 770, 260);
-INSERT INTO Politiebureaus VALUES ('Zuidplein','Zuidplein 111','Charlois', 917, 330);
+INSERT INTO Politiebureaus VALUES ('Zuidplein','Zuidplein 111','Charlois', 917, 440);
 INSERT INTO Politiebureaus VALUES ('De Veranda','Cor Kieboomplein 475','Feijenoord', 990, 410);
 INSERT INTO Politiebureaus VALUES ('Sandelingplein','Groene Hilledijk 275','Feijenoord', 950, 438);
 INSERT INTO Politiebureaus VALUES ('J.J.P. Oudsingel','Dr. JJP Oudsingel 1','Prins Alexander', 1058, 205);
