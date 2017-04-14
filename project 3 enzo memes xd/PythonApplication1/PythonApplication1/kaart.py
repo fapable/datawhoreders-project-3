099f5e5adac3366b59257974be0c388d92528ed9
fapable <ramonvanek@outlook.com>
2017-04-14 15:14:15 +0200

    Add files via upload

--- project 3 enzo memes xd/PythonApplication1/PythonApplication1/kaart.py
+++ project 3 enzo memes xd/PythonApplication1/PythonApplication1/kaart.py
@@ -102,7 +102,7 @@ if d.get_data("average", "criminaliteit", "'2009'", "'Charlois'") > 10:
 
 def mainLoop():
     #politiebureau()
-    button1.grid(row = 0, column = 0, pady = 20)
+    button1.grid(row = 0, column = 0)
     button2.grid(row = 1, column = 0)
     button3.grid(row = 2, column = 0)
     canvas.grid(row=3, column=1)
