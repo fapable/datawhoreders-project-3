9d4c0114bb357435a8c849d054b625ffb42c79c7
Ino van den Berg <0928787@hr.nl>
2017-04-14 15:57:09 +0200

    Add files via upload

--- project 3 enzo memes xd/PythonApplication1/PythonApplication1/database.py
+++ project 3 enzo memes xd/PythonApplication1/PythonApplication1/database.py
@@ -1,14 +1,12 @@
 import psycopg2
 
 class Database:  
-     def interact_with_database(command):
-        connection = psycopg2.connect(host = "localhost", dbname = "Project 3", user = "postgres", password = "61hhsjmercedes")
+    def interact_with_database(command):
+        connection = psycopg2.connect(host = "localhost", dbname = "Jaar 1 Project 3", user = "postgres", password = "DatThaBeast3636")
         cursor = connection.cursor()
-
         # Execute the command
         cursor.execute(command)
         connection.commit()
-
         # Save results
         results = None
         try:
@@ -16,12 +14,13 @@ class Database:
         except psycopg2.ProgrammingError:
             # Nothing to fetch
             pass
-
         # Close connection
         cursor.close()
         connection.close()
-
         return results
 
-def get_data(result, tabel, jaar, wijk):
-   return Database.interact_with_database("SELECT " + str(result) + " FROM " + str(tabel) + " WHERE jaar = " + str(jaar) + " AND wijk = " + str(wijk))[0][0]
+    def get_crime_data(result, jaar, wijk):
+        return Database.interact_with_database("SELECT " + str(result) + " FROM criminaliteit WHERE jaar = " + str(jaar) + " AND wijk = " + str(wijk))[0][0]
+
+    def politie_coordinaten():
+        return Database.interact_with_database("SELECT X, Y FROM politiebureaus")
--- project 3 enzo memes xd/PythonApplication1/PythonApplication1/kaart.py
+++ project 3 enzo memes xd/PythonApplication1/PythonApplication1/kaart.py
@@ -1,8 +1,8 @@
 from tkinter import *
 import colors as c
 import pliesiebureaus as pb
-import database as d
-    
+from database import Database as d
+import matplotlib    
         
         
 
@@ -34,33 +34,14 @@ class Kaart:
 def callback():
     print("click!")
 
-b = Button(tk, text="OK", command=callback)
 button1 = Button(tk, text ="Question 1", command = callback, bg = "white", fg = "black")
-
 button2 = Button(tk, text ="Question 2", command = callback, bg = "white", fg = "black")
-
 button3 = Button(tk, text ="Question 3", command = callback, bg = "white", fg = "black")
-button3.grid(row = 2, column = 0)
-
 
 
 def politiebureau():
-    canvas.create_image(895, 305, image = pimg) #Doelwater (Hoofdbureau)
-    canvas.create_image(900, 260, image = pimg) #Zaagmolenstraat
-    canvas.create_image(925, 292, image = pimg) #Bureau Boezemsingel
-    canvas.create_image(897, 230, image = pimg) #Noord
-    canvas.create_image(960, 297, image = pimg) #Taborstraat
-    canvas.create_image(926, 390, image = pimg) #Maashaven
-    canvas.create_image(780, 345, image = pimg) #Marconiplein
-    canvas.create_image(770, 260, image = pimg) #Overschie
-    canvas.create_image(917, 440, image = pimg) #Zuidplein
-    canvas.create_image(990, 410, image = pimg) #De Veranda
-    canvas.create_image(950, 438, image = pimg) #Sandelingplein
-    canvas.create_image(1058, 205, image = pimg) #J.J.P. Oudsingel
-    canvas.create_image(655, 310, image = pimg) #Schiedam
-    canvas.create_image(1138, 292, image = pimg) #Capelle a/d IJssel
-    #er zijn nog 12 bureau's meer, maar die zijn niet van toepassing op onze gegevens. als iemand zich verveelt kan hij/zij het maken. vanaf O. https://www.politie.nl/mijn-buurt/politiebureaus?&geoquery=rotterdam&distance=10.0
-
+    for item in d.politie_coordinaten():
+        canvas.create_image(item[0], item[1], image = pimg)
 
 class Area:
     def __init__(self,color,location):
@@ -68,20 +49,6 @@ class Area:
         self.shape = canvas.create_polygon(location, fill=self.color, outline='black', width = 2)
 
 map = Kaart()
-
-
-#720p
-        #charlois = Area("blue",(841,409,859,428,919,411,915,463,907,465,907,480,945,515,971,549,925,547,877,565,877,579,828,581,774,559,801,549,812,533,801,528,799,509,831,437,817,431,818,413))
-        #delfshaven = Area("blue",(705,319,689,364,699,388,692,407,742,398,818,413,841,409,829,372,815,369,831,359,829,304))
-        #feijenoord = Area("blue",(931,333,917,337,881,383,841,409,859,428,919,411,915,463,907,465,907,480,945,515,969,490,991,490,997,465,967,403,979,395,969,348))
-        #hillegersberg_schiebroek = Area("blue",(828,123,825,237,1013,188,999,126,983,142,955,130,952,115,925,94,921,103,853,60))
-        #ijsselmonde = Area("blue",(1048,396,993,411,979,395,967,403,997,465,991,490,969,490,945,515,971,549,1016,554,1049,542,1105,511,1111,519,1121,513,1141,404,1083,389))
-        #kralingen_crooswijk = Area("blue",(1014,188,1041,303,1025,332,1048,396,993,411,979,395,969,348,931,333,921,307,891,303,887,220))
-        #noord = Area("blue",(887,220,825,237,739,288,757,312,804,308,805,303,859,295,891,303))
-        #overschie = Area("blue",(603,129,631,122,637,137,737,73,784,133,815,109,828,123,825,237,739,288,759,314,705,319,678,269,706,233,700,230,669,244,643,215,633,228))
-        #prins_alexander = Area("blue",(1041,303,1107,283,1101,233,1129,251,1100,151,1109,148,1115,155,1214,94,1194,77,1210,21,1203,8,1078,51,1120,97,1085,113,1063,89,999,126))
-        #centrum = Area("blue",(841,409,829,372,815,369,831,359,829,307,804,308,805,303,859,295,891,303,921,307,931,333,917,337,881,383))
-
 
 #1080p
 #overschie = polygon(c.red0(),(904,194,947,183,955,205,1106,109,1176,199,1222,163,1242,185,1237,355,1109,432,1138,471,1057,479,1017,403,1059,350,1050,345,1003,366,964,322,949,342))
@@ -97,17 +64,17 @@ map = Kaart()
 
 #pb.politiebureau()
 
-if d.get_data("average", "criminaliteit", "'2009'", "'Charlois'") > 10:
+if d.get_crime_data("average", "'2009'", "'Charlois'") > 10:
     canvas.itemconfig(map.charlois.shape, fill="black")
 
 def mainLoop():
-    #politiebureau()
+    politiebureau()
+    canvas.grid()
     button1.grid(row = 0, column = 0)
     button2.grid(row = 1, column = 0)
     button3.grid(row = 2, column = 0)
     canvas.grid(row=3, column=1)
-
-    #b.grid()
     tk.mainloop() 
+    d.test()
 
 mainLoop()
