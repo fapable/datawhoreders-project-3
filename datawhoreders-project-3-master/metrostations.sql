e8938deee6e1058f98acac3a80c857eeabeb6113
fapable <ramonvanek@outlook.com>
2017-04-17 16:45:05 +0200

    Add files via upload

---
+++ datawhoreders-project-3-master/metrostations.sql
@@ -0,0 +1,40 @@
+CREATE TABLE metrostations (
+    X int,
+    Y int);
+    
+
+INSERT INTO metrostations VALUES (1070, 205);
+INSERT INTO metrostations VALUES (1120, 145);
+INSERT INTO metrostations VALUES (905, 330);
+INSERT INTO metrostations VALUES (1040, 165);
+INSERT INTO metrostations VALUES (925, 325);
+INSERT INTO metrostations VALUES (847, 283);
+INSERT INTO metrostations VALUES (840, 363);
+INSERT INTO metrostations VALUES (820, 363);
+INSERT INTO metrostations VALUES (863, 355);
+INSERT INTO metrostations VALUES (890, 335);
+INSERT INTO metrostations VALUES (955, 310);
+INSERT INTO metrostations VALUES (1070, 175);
+INSERT INTO metrostations VALUES (1080, 148);
+INSERT INTO metrostations VALUES (1030, 320);
+INSERT INTO metrostations VALUES (900, 350);
+INSERT INTO metrostations VALUES (932, 415);
+INSERT INTO metrostations VALUES (773, 355);
+INSERT INTO metrostations VALUES (847, 185);
+INSERT INTO metrostations VALUES (850, 205);
+INSERT INTO metrostations VALUES (1160, 90);
+INSERT INTO metrostations VALUES (1100, 145);
+INSERT INTO metrostations VALUES (1075, 220);
+INSERT INTO metrostations VALUES (940, 313);
+INSERT INTO metrostations VALUES (1078, 240);
+INSERT INTO metrostations VALUES (938, 390);
+INSERT INTO metrostations VALUES (1055, 160);
+INSERT INTO metrostations VALUES (865, 310);
+INSERT INTO metrostations VALUES (1095, 270);
+INSERT INTO metrostations VALUES (715, 325);
+INSERT INTO metrostations VALUES (890, 500);
+INSERT INTO metrostations VALUES (885, 320);
+INSERT INTO metrostations VALUES (1135, 125);
+INSERT INTO metrostations VALUES (970, 315);
+INSERT INTO metrostations VALUES (930, 375);
+INSERT INTO metrostations VALUES (915, 455);
