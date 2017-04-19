7dd11637be38e845b44baca6c26c68dbe00d1fae
fapable <ramonvanek@outlook.com>
2017-04-19 15:51:17 +0200

    Update marktlocaties.sql

--- marktlocaties.sql
+++ marktlocaties.sql
@@ -3,12 +3,12 @@ X INT,
 Y INT
 );
 
-INSERT INTO marktlocaties VALUES(405,505);
+INSERT INTO marktlocaties VALUES(405,305);
 INSERT INTO marktlocaties VALUES(405,315);
-INSERT INTO marktlocaties VALUES(380,399);
+INSERT INTO marktlocaties VALUES(380,333);
 INSERT INTO marktlocaties VALUES(443,405);
 INSERT INTO marktlocaties VALUES(580,160);
-INSERT INTO marktlocaties VALUES(380,360);
-INSERT INTO marktlocaties VALUES(260,270);
+INSERT INTO marktlocaties VALUES(305,360);
+INSERT INTO marktlocaties VALUES(380,160);
 INSERT INTO marktlocaties VALUES(400,505);
 INSERT INTO marktlocaties VALUES(472,315);
