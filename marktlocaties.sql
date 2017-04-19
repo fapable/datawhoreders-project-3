e09af547a9758f4147efdf6914ee87c7c43d383c
fapable <ramonvanek@outlook.com>
2017-04-19 14:15:49 +0200

    Add files via upload

---
+++ marktlocaties.sql
@@ -0,0 +1,13 @@
+CREATE TABLE marktlocaties(
+X INT,
+Y INT
+);
+
+INSERT INTO marktlocaties VALUES(405,505);
+INSERT INTO marktlocaties VALUES(405,315);
+INSERT INTO marktlocaties VALUES(380,399);
+INSERT INTO marktlocaties VALUES(443,405);
+INSERT INTO marktlocaties VALUES(580,160);
+INSERT INTO marktlocaties VALUES(380,360);
+INSERT INTO marktlocaties VALUES(260,280);
+INSERT INTO marktlocaties VALUES(400,505);
